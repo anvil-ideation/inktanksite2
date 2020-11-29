a04e06ce99f1c9748294fc7d3791fb853e1d1db8
anvil-ideation <landon@anvilideation.com>
2020-11-28 22:27:27 -0700

    Books List Start Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="../public/assets/image/logo.svg" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
